385aca7c5746242cfa967a8f3ff7c611a08cad1a
muhmdpp <connect.mhdpp@gmail.com>
2024-07-24 21:35:13 +0530

    newsSection - Structure Completed

--- aaga-native/app/news.jsx
+++ aaga-native/app/news.jsx
@@ -1,10 +1,19 @@
-import { View, Text,StyleSheet } from 'react-native'
+import { View, Text,StyleSheet,ScrollView ,Image} from 'react-native'
 import React from 'react'
-
+import Thumbnail from '../assets/images/newsThumb.png'
 export default function news() {
   return (
     <View style={styles.container}> 
-      <Text style={styles.text}>News</Text>
+    <ScrollView>
+      <View style={styles.mainBox}>
+        <View style={styles.contentArea}></View>
+        <View style={styles.photoBox}>
+          <Image source={Thumbnail} style={{width:'100%',height:'100%',objectFit:'cover'}}/>
+        </View>
+      </View>
+      
+
+      </ScrollView>
     </View>
   )
 }
@@ -20,5 +29,34 @@ const styles= StyleSheet.create({
   },
   text:{
     color:'#fff'
+  },
+  mainBox:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'flex-end',
+    alignItems:'center',
+    width:380,
+    height:100,
+    backgroundColor:'#091e1a',
+    marginTop:15,
+    // borderCurve:10,
+    borderRadius:15,
+    overflow:'hidden'
+
+  },
+  photoBox:{
+    height:85,
+    width:100,
+    backgroundColor:'#ff0000',
+    alignItems:'center',
+    marginRight:8,
+    borderRadius:10,
+    overflow:'hidden'
+
+  },
+  contentArea:{
+    height:85,
+    width:280,
+    backgroundColor:'#ff0000',
   }
 })
