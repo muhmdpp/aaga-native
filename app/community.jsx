341947f0fa54bfe0eeb551519ed36648ed81b073
muhmdpp <connect.mhdpp@gmail.com>
2024-08-16 12:00:06 +0400

    communityStructure-Completed

--- app/community.jsx
+++ app/community.jsx
@@ -1,11 +1,18 @@
-import { View, Text, StyleSheet, ScrollView, Image, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  ImageBackground,
+} from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import users from "../constants/users.json";
 import mhd from "../assets/images/mhdpp.png";
-import danny from "../assets/images/danny.png"
-import heen from "../assets/images/heen.png"
-import suhl from "../assets/images/suhl.png"
+import danny from "../assets/images/danny.png";
+import heen from "../assets/images/heen.png";
+import suhl from "../assets/images/suhl.png";
 const community = () => {
   return (
     <View style={styles.container}>
@@ -20,27 +27,44 @@ const community = () => {
           </View>
         </View>
         <View>
-          <Text style={{fontWeight:'bold',color:'#fff',marginLeft:12,marginTop:10}}>STORIES</Text>
+          <Text
+            style={{
+              fontWeight: "bold",
+              color: "#fff",
+              marginLeft: 12,
+              marginTop: 10,
+            }}
+          >
+            STORIES
+          </Text>
         </View>
         <View style={styles.storySection}>
           <ScrollView horizontal={true}>
             <View style={styles.storyBoxFirst}>
-              <ImageBackground  source={mhd} style={{ width: "100%", height: "100%" ,display:'flex',justifyContent:'flex-end',alignItems:'flex-end'}}>
-                  <View style={styles.addPlus}>
-                    <Ionicons name="add"  size={20} color={"#fff"}/>
-                  </View>
+              <ImageBackground
+                source={mhd}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  alignItems: "flex-end",
+                }}
+              >
+                <View style={styles.addPlus}>
+                  <Ionicons name="add" size={20} color={"#fff"} />
+                </View>
               </ImageBackground>
             </View>
 
             <View style={styles.storyBox}>
               <Image source={heen} style={{ width: "100%", height: "100%" }} />
-              
             </View>
-            
+
             <View style={styles.storyBox}>
               <Image source={danny} style={{ width: "100%", height: "100%" }} />
             </View>
-            
+
             <View style={styles.storyBox}>
               <Image source={suhl} style={{ width: "100%", height: "100%" }} />
             </View>
@@ -48,36 +72,74 @@ const community = () => {
             <View style={styles.storyBox}>
               <Image source={heen} style={{ width: "100%", height: "100%" }} />
             </View>
-            
+
             <View style={styles.storyBox}>
               <Image source={danny} style={{ width: "100%", height: "100%" }} />
             </View>
-            
+
             <View style={styles.storyBox}>
               <Image source={suhl} style={{ width: "100%", height: "100%" }} />
             </View>
-
-            
-           
-
           </ScrollView>
         </View>
         {/* Post Section */}
-          <View style={styles.postContainer}>
-            
-            <View style={styles.post}>
+        <View style={styles.postContainer}>
+          <View style={styles.post}>
             <View style={styles.postHeader}>
-              <View style={styles.profilePic}></View>
-              <View style={styles.idDetails}>
-                <View style={styles.idorname}></View>
-                <View style={styles.timeDetails}></View>
+              <View style={{ display: "flex", flexDirection: "row" }}>
+                <View style={styles.profilePic}>
+                  <Image
+                    source={danny}
+                    style={{ width: "100%", height: "100%", borderRadius: 50 }}
+                  />
+                </View>
+                <View style={styles.idDetails}>
+                  <Text
+                    style={{
+                      fontSize: 17,
+                      color: "#fff",
+                      fontWeight: "bold",
+                      margin: 8,
+                      marginTop: 3,
+                    }}
+                  >
+                    Firas Ahmed Daniyal
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      color: "#fff",
+                      marginLeft: 8,
+                      marginTop: 0,
+                      bottom: 10,
+                    }}
+                  >
+                    1 hour ago
+                  </Text>
+                </View>
+              </View>
+              <View style={{ marginRight: 15 }}>
+                <Ionicons
+                  name="ellipsis-horizontal"
+                  size={20}
+                  color={"#fff"}
+                  style={{ right: 0 }}
+                />
               </View>
             </View>
-            <View style={styles.PostContent}></View>
-            <View style={styles.postFooter}></View>
+            <View style={styles.PostContent}>
+                      <Text style={{color:'#fff',fontSize:26}}>Hello World, How are you, Im fine Hello World, How are you, Im fine Hello World, How are you, Im fine</Text>
+            </View>
+            <View style={styles.postFooter}>
+                      <View style={styles.iconsArea}>
+                        <Ionicons name="heart-outline" color={"#fff"} size={30}/>
+                        <Ionicons name="chatbubble-outline" color={"#fff"} size={30}/>
+                        <Ionicons name="arrow-redo-outline" color={"#fff"} size={30}/>
+                      </View>
+            </View>
           </View>
           {/* Post Section */}
-      </View>
+        </View>
       </View>
     </View>
   );
@@ -104,7 +166,7 @@ const styles = StyleSheet.create({
   },
   searchSection: {
     width: "100%",
-    height:60,
+    height: 60,
     backgroundColor: "#0e201d",
     display: "flex",
     flexDirection: "row",
@@ -118,8 +180,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height: 80,
-    marginTop:8,
-    
+    marginTop: 8,
   },
   camera: {
     display: "flex",
@@ -136,61 +197,90 @@ const styles = StyleSheet.create({
     borderRadius: 25,
   },
   storyBox: {
-    display:'flex',
-    flexDirection:'column',
+    display: "flex",
+    flexDirection: "column",
     backgroundColor: "#fff",
     width: 75,
     height: 75,
     borderRadius: 75,
     marginLeft: 10,
-    borderWidth:3,
-    borderColor:'rgba(217,220,167,0.4)',
-    marginBottom:2
+    borderWidth: 3,
+    borderColor: "rgba(217,220,167,0.4)",
+    marginBottom: 2,
   },
-  storyBoxFirst:{
+  storyBoxFirst: {
     backgroundColor: "#fff",
     width: 75,
     height: 75,
     borderRadius: 75,
     marginLeft: 10,
-    borderWidth:3,
-    borderColor:'rgba(217,220,167,0.4)',
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center'
+    borderWidth: 3,
+    borderColor: "rgba(217,220,167,0.4)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  addPlus:{
-    width:25,
-    height:25,
-    backgroundColor:'#358d00',
-    borderRadius:25,
-    display:'flex',justifyContent:'center',alignItems:'center'
+  addPlus: {
+    width: 25,
+    height: 25,
+    backgroundColor: "#358d00",
+    borderRadius: 25,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  postContainer:{
-    marginTop:5,
-    paddingTop:8,
+  postContainer: {
+    marginTop: 5,
+    paddingTop: 8,
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "auto",
+    backgroundColor: "#0e201d",
+  },
+  postHeader: {
+    paddingTop: 10,
+    display: "flex",
+    flexDirection: "row",
+    height: 50,
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // backgroundColor:'#ff0000'
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    // backgroundColor:'#fff',
+    marginLeft: 8,
+  },
+  idDetails: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  post: {
+    backgroundColor: "#10332d",
+  },
+  postFooter:{
+    width:'100%',
+    height:40,
+    // backgroundColor:'#fff'
+    borderTopWidth:0.7,
+    borderTopColor:'#999999'
+  },
+  iconsArea:{
     display:'flex',
-    flexDirection:'column',
+    flexDirection:'row',
+    width: 150,
+    height:'100%',
+    // backgroundColor:'#ff0000',
+    alignItems:'center',
+    justifyContent:'space-around',
+    
+  },
+  PostContent:{
     width:'100%',
     height:'auto',
-    backgroundColor:'#0e201d'
-  },
-  postHeader:{
-    height:50,
-    width:'100%',
-    backgroundColor:'#ff0000'
-  },
-  profilePic:{
-    width:50,
-    height:50,
-    backgroundColor:'#fff',
-    marginLeft:8
-  },
-  idDetails:{
-    display:'flex',
-    flexDirection:'column'
-  },
-  idorname:{
-    
+    padding:12
   }
 });
