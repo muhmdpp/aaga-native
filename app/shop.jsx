e450ddb8b7cf69525eba5871c568c03e26ad13cf
muhmdpp <connect.mhdpp@gmail.com>
2024-08-16 21:06:19 +0400

    All Basic Pages completed

--- app/shop.jsx
+++ app/shop.jsx
@@ -1,9 +1,10 @@
-import { View, Text,StyleSheet, Image,ScrollView} from 'react-native'
+import { View, Text,StyleSheet, Image,ScrollView, VirtualizedList, ImageBackground} from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 import banner1 from '../assets/images/banner1.jpg'
 import garden from "../assets/images/garden.jpg"
+import pp from "../assets/images/p1.jpg"
 const shop = () => {
   return (
     <View style={styles.container}> 
@@ -25,19 +26,152 @@ const shop = () => {
     
       <View style={styles.filters}>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
-        <Text style={styles.filter}>All</Text>
+          <View style={styles.filterFOCUS}>
+        <Text style={styles.filterTextFOCUS}>All</Text>
+        </View>
+
+        <View style={styles.filter}>
+        <Text style={styles.filterText}>Indoor</Text>
+        </View>
+
+        <View style={styles.filter}>
+        <Text style={styles.filterText}>Outdoor</Text>
+        </View>
+
+        <View style={styles.filter}>
+        <Text style={styles.filterText}>Garden</Text>
+        </View>
+
+        <View style={styles.filter}>
+        <Text style={styles.filterText}>Office</Text>
+        </View>
+        
+       
         </ScrollView>
       </View>
     </View>
 
+    <View style={styles.midTitle}>
+      <Text style={{color:"#fff",fontSize:20,fontWeight:'bold',padding:15}}>Recommended for You</Text>
+      <Text style={{color:"#fff",fontSize:15,padding:16}}>see all</Text>
+    </View>
+    <ScrollView>
+    <View style={styles.productScrollSection}>
+      
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={pp} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+      
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
+
+      <View style={styles.productBox}>
+        <View style={styles.photoArea}>
+          <Image source={garden} style={styles.imageFit} />
+            
+        </View>
+        <View style={styles.PlantDesc}>
+          <View style={{marginTop:10}}>
+          <Text style={{fontSize:16}}>Aloe Vera</Text>
+          <Text style={{fontWeight:'bold',fontSize:17}}>Rs.1234.00</Text>
+          </View>
+         <Ionicons name='add-circle-outline' size={30} style={{marginTop:10}} color={"#10332d"}/>
+        </View>
+      </View>
    
+    </View>
+    </ScrollView>
   
   </View>
   )
@@ -100,23 +234,87 @@ const styles= StyleSheet.create({
     alignItems:'center'
   },
   filter:{
+    display:'flex',
+    justifyContent:'center',
+    flexDirection:'row',
+    alignItems:'center',
     height:35,
-    width:65,
-    padding:3,
+    minWidth:65,
+    width:'auto',
     borderWidth:1,
     borderColor:'rgba(255, 255, 255, 0.2)',
     
     borderRadius:35,
-    marginLeft:10,
-    fontSize:20,
-    paddingLeft:20,
+    marginLeft:10
+  },
+  PlantDesc:{
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-around',
+    // marginBottom:4
+  },
+  filterText:{
+    fontSize:18,
     color:"#fff",
-    opacity:0.7
-
+    opacity:0.7,
+    paddingLeft:10,
+    paddingRight:10
+  },
+  filterTextFOCUS:{
+    fontSize:18,
+    color:"#000",
+    opacity:0.7,
+    paddingLeft:10,
+    paddingRight:10
+  },
+  filterFOCUS:{
+    display:'flex',
+    justifyContent:'center',
+    flexDirection:'row',
+    alignItems:'center',
+    height:35,
+    minWidth:65,
+    width:'auto',
+    borderWidth:1,
+    backgroundColor:"#fff",
+    borderColor:'rgba(255, 255, 255, 0.2)',
     
+    borderRadius:35,
+    marginLeft:10,
   },
   filters:{
     display:"flex",
     flexDirection:'row'
+  },
+  midTitle:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    width:'100%',
+    alignItems:'center'
+  },
+  productBox:{
+    width:170,
+    height:200,
+    backgroundColor:'#d9dca7',
+    margin:10,
+    borderRadius:24,
+    overflow:'hidden'
+  },
+  productScrollSection:{
+    display:'flex',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'center',
+    width:'100%',
+    // height:400
+  },
+  photoArea:{
+    width:170,
+    height:140,
+    borderBottomEndRadius:20,
+    borderBottomLeftRadius:20,
+    overflow:'hidden'
   }
 })
